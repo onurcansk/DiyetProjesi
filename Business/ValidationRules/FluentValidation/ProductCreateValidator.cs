0125a5f2bc819bbc13a4f41e327dd0c952fbb532
AliEren <erenadas@gmail.com>
2022-11-23 12:22:32 +0300

    Feriha

--- Business/ValidationRules/FluentValidation/ProductCreateValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductCreateValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Dtos.Product;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace Business.ValidationRules.FluentValidation
 {
@@ -7,7 +8,32 @@ namespace Business.ValidationRules.FluentValidation
     {
         public ProductCreateValidator()
         {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Yemek adı boş olamaz.");
+            RuleFor(x => x.ProductName).NotNull().WithMessage("Yemek adı boş olamaz.");
+            RuleFor(x => x.ProductName).MaximumLength(30).WithMessage("Yemek adı 30 karakterden büyük olamaz");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("Yemek adı 2 karakterden küçük olamaz");
+            RuleFor(x => x.ProductName).Must(isContainsOnlyLetters).WithMessage("Yemek adı özel karakter veya rakam içermemelidir.");
 
+            RuleFor(u => u.UnitCalorie).GreaterThanOrEqualTo(0).WithMessage("Ölçülebilir bir kalori miktarı belirtiniz.");
+            RuleFor(u => u.UnitCalorie).LessThan(20).WithMessage("Lütfen girilen miktarı kontrol ediniz.");
+            RuleFor(u => u.UnitCalorie).NotNull().WithMessage("Birim kalori boş bırakılamaz.");
+
+            RuleFor(x => x.ProductType).NotNull().WithMessage("Yemek türü boş olamaz.");
+            RuleFor(x => x.ProductType).NotEmpty().WithMessage("Yemek türü boş olamaz.");
+            RuleFor(x => x.ProductType).MaximumLength(30).WithMessage("Yemek türü 30 karakterden büyük olamaz");
+            RuleFor(x => x.ProductType).MinimumLength(4).WithMessage("Yemek türü 4 karakterden küçük olamaz");
+            RuleFor(x => x.ProductType).Must(isContainsOnlyLetters).WithMessage("Yemek türü özel karakter veya rakam içermemelidir.");
+
+   
         }
+        private bool isContainsOnlyLetters(string productName)
+        {
+            return Regex.IsMatch(productName, @"^[a-zA-Z]+$");
+        }
+        
+
+
+
+
     }
 }
